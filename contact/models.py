8b82171413b38cb97af99bb944008020e6c3dbf3
arnaldo felipe <feelipesilvaa11@gmail.com>
2025-06-07 16:14:17 -0300

    atualizando html

--- contact/models.py
+++ contact/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.last_name
+        return self.name
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
