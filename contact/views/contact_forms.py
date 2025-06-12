e7c64b2bf75cb748960c369e58c877875df44c79
arnaldo felipe <feelipesilvaa11@gmail.com>
2025-06-12 17:58:05 -0300

    finalizando o projeto, organizando or forms, urls, e as funcionalidades

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -4,9 +4,11 @@ from django.urls import reverse
 from contact.forms import ContactForm
 from contact.models import Contact
 
+from django.contrib.auth.decorators import login_required
 
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -19,7 +21,9 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
             
 
@@ -41,9 +45,9 @@ def create(request):
         context
     )
 
-
+@login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True,)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user,)
     form_action = reverse('contact:update', args=(contact_id,))
 
 
@@ -76,6 +80,7 @@ def update(request, contact_id):
         context
     )
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id, show=True,)
 
